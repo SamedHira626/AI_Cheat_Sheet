5fbc4f75c403aa1f02d3b04f43fe196a94162b78
Samed Hıra <samedhira626@gmail.com>
2022-01-25 16:27:45 +0300

    Update proj_2_inheritance.py

--- OOP all/proj_2_inheritance.py
+++ OOP all/proj_2_inheritance.py
@@ -5,7 +5,7 @@ class Website:
         self.surname = surname
     
     def loginInfo(self):
-        print(self.name + " "+ self.surname )
+        print(self.name + " " + self.surname )
 
 class Website1(Website):
     "child"
@@ -14,7 +14,7 @@ class Website1(Website):
         self.ids = ids
     
     def login(self):
-        print(self.name + " "+ self.surname + " "+self.ids)
+        print(self.name + " " + self.surname + " " + self.ids)
 class Website2(Website):
 
     def __init__ (self, name, surname, email):
@@ -22,8 +22,15 @@ class Website2(Website):
         self.email = email
 
     def login(self):
-        print(self.name + " "+ self.surname + " "+self.email)
+        print(self.name + " " + self.surname + " " + self.email)
           
 p1 = Website("name","surname")
 p2 = Website1("name","surname", "123")
 p3 = Website2("name","surname", "email@")
+
+p1.loginInfo()
+print("---")
+p2.login()
+p2.loginInfo()
+# p2.name
+# p2.surname
