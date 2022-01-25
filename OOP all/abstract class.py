1bfc0d19f095be729a00c848e5140ce5f926ccbc
Samed Hıra <samedhira626@gmail.com>
2022-01-25 17:01:11 +0300

    explained

--- OOP all/abstract class.py
+++ OOP all/abstract class.py
@@ -1,8 +1,9 @@
-from abc import ABC, abstractmethod
-
-class Animal(ABC): # super class
+from abc import ABC, abstractmethod  # abc is abstract base class
+# We cannot create an object from Animal class, this is the first rule of abstract class 
+# We must use functions we used in superclass in the subclass 
+class Animal(ABC): # super class                    
     
-    @abstractmethod
+    @abstractmethod      # abstract method, child class'ta implement etmek zorunda olduğun methodlara denir
     def walk(self): pass
 
     def run(self): pass
