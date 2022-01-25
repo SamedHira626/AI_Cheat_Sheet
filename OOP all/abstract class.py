a123a2ea5ccd8fb41514104ddc22fe5cd433bb7f
Samed Hıra <samedhira626@gmail.com>
2022-01-25 17:03:06 +0300

    Update abstract class.py

--- OOP all/abstract class.py
+++ OOP all/abstract class.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod  # abc is abstract base class
-# We cannot create an object from Animal class, this is the first rule of abstract class 
+# We cannot create an object from Animal class (abstract class), this is the first rule of abstract class, any object cannot be created by abstract class
 # We must use functions we used in superclass in the subclass 
 class Animal(ABC): # super class                    
     
