ae8901f5064538f1f04279d6ad28d550b3c0ddb5
Samed Hıra <samedhira626@gmail.com>
2022-02-20 01:46:01 +0300

    commented

--- OpenCV/2_OpenCV Image Processing/7_warp perspective/7_perspektif çarpıtma.py
+++ OpenCV/2_OpenCV Image Processing/7_warp perspective/7_perspektif çarpıtma.py
@@ -8,8 +8,8 @@ cv2.imshow("Orijinal", img)
 width = 400
 height = 500
 
-pts1 = np.float32([[230,1],[1,472],[540,150],[338,617]])
-pts2 = np.float32([[0,0],[0, height],[width,0],[width,height]])
+pts1 = np.float32([[230,1],[1,472],[540,150],[338,617]]) # first location of 4 corners
+pts2 = np.float32([[0,0],[0, height],[width,0],[width,height]]) # desired location
 
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
 print(matrix)
