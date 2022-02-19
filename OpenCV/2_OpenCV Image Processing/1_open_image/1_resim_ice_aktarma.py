9b6595ee264acd8bb19ec94b9adcebbbab527992
Samed Hıra <samedhira626@gmail.com>
2022-02-19 18:46:20 +0300

    commented

--- OpenCV/2_OpenCV Image Processing/1_open_image/1_resim_ice_aktarma.py
+++ OpenCV/2_OpenCV Image Processing/1_open_image/1_resim_ice_aktarma.py
@@ -1,16 +1,16 @@
 import cv2
 
 # içe aktarma
-img = cv2.imread("messi5.jpg", 0)
+img = cv2.imread("messi5.jpg", 0) # 0 means gray scale so it consists of black n white
 
 # görselleştir
 cv2.imshow("ilk Resim", img)
 
-k = cv2.waitKey(0) &0xFF
+k = cv2.waitKey(0) &0xFF    # wait for keyboard
 
-if k == 27: # wsc
+if k == 27: # esc
     cv2.destroyAllWindows()
-elif k == ord('s'):
+elif k == ord('s'):  # if 's' pressed, save the image
     cv2.imwrite("messi_gray.png", img)
     cv2.destroyAllWindows()
     
