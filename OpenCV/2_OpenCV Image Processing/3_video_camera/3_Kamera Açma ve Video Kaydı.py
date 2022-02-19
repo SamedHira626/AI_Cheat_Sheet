7dfb394e55d213338bcb5726aeaccfbb74b43087
Samed Hıra <samedhira626@gmail.com>
2022-02-19 18:49:33 +0300

    commented

--- OpenCV/2_OpenCV Image Processing/3_video_camera/3_Kamera Açma ve Video Kaydı.py
+++ OpenCV/2_OpenCV Image Processing/3_video_camera/3_Kamera Açma ve Video Kaydı.py
@@ -8,17 +8,18 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(width, height)
 
 # video kaydet
-writer = cv2.VideoWriter("video_kaydı.mp4", cv2.VideoWriter_fourcc(*"DIVX"),20,(width, height))
+writer = cv2.VideoWriter("video_kaydı.mp4", cv2.VideoWriter_fourcc(*"DIVX"),20,(width, height)) # 20 fps, DIVX for windows
 
 while True:
     
-    ret, frame = cap.read()
+    ret, frame = cap.read() # ret return true or false, frame returns width, height 
     cv2.imshow("Video",frame)
     
     # save
-    writer.write(frame)
+    writer.write(frame)  # save frames
     
-    if cv2.waitKey(1) & 0xFF == ord("q"): break
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
 cap.release()
 writer.release()
